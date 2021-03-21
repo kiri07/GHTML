8af5882b3c8863facbf20ecc9fce7e935bbcf8fd
kiri07 <merlinalessandroandrea@gmail.com>
2021-03-21 16:12:50 +0100

    added Inline tables for DB
    Tables are now inline, working with vectors, points in canvas grid

--- WebContent/js/classdiagramRender.js
+++ WebContent/js/classdiagramRender.js
@@ -1,39 +1,61 @@
 var ClassDiagramRenderer = 
 	{
-		name:
-			"",
-		attributos:
-			[],
-		methods:
+		items:
 			[],
 		render:
-			function(document, id, name, attributos, methods)
+			function(document, id, mapping)
 			{
-				this.name = name ? name : "noname";
-				this.attributos = attributos ? attributos.split(",") : [];
-				this.methods = methods ? methods.split(",") : [];
-				const canvas = document.getElementById(id);
-				const context = canvas.getContext("2d");
-				context.fillStyle = 'red';
-				context.strokeStyle = 'black';
-				
-				context.font = '20pt Verdana';
-				context.fillText(name, 10, 50);
-				context.strokeText(name, 10, 50);
-				context.fill();
-				context.stroke();
-				var txt = "";
-				var startPos = 70;
-				for (var i = 0; i < this.attributos.length; i ++)
+				var texto = mapping.split("\n");
+				var startx = 10;
+				var starty = 30;
+				for (var i = 1; i < texto.length; i++)
 				{
-					txt = this.attributos[i] + "\n";
-					context.font = '10pt Calibri';
-					context.fillText(txt, 10, startPos);
-					context.strokeText(txt, 10, startPos);
-					context.fill();
-					context.stroke();
-					startPos += 20;
-				}
+					console.log(texto[i].substring(3,4));
+					if(texto[i].substring(3,4) == "C")
+					{
+						console.log(texto[i].split(";")[0],texto[i].split(";")[1],startx,starty);
+						this.items.push(new item(texto[i].split(";")[1],texto[i].split(";")[2],startx,starty));
+					}
+					startx += 300;
+				};
+				for (var i = 0; i < this.items.length; i++)
+				{
+					const canvas = document.getElementById(id);
+					const context = canvas.getContext("2d");
+					context.fillStyle = 'red';
+					context.strokeStyle = 'black';
+					
+					context.font = '20pt Verdana';
+					context.fillText(this.items[i].name, this.items[i].posx, this.items[i].posy);
+					context.strokeText(name, 10, 50);
+					this.items[i].posy += 30;
+					 for (var j = 0 ; j < this.items[i].attributes.length; j++)
+					{
+						const canvas = document.getElementById(id);
+						const context = canvas.getContext("2d");
+						context.fillStyle = 'red';
+						context.strokeStyle = 'black';
+						var content = "";
+						console.log(content);
+						content = this.items[i].attributes[j] + "\n";
+						context.font = '10pt Verdana';
+						context.fillText(content, this.items[i].posx, this.items[i].posy);
+						context.strokeText(content, this.items[i].posx, this.items[i].posy);
+						this.items[i].posy += 30;
+					}; 
+				};
 					
 			}
-};
+	};
+	
+class item
+{
+	constructor(name,attributes, posx, posy)
+	{
+		this.name = name;
+		this.attributes = attributes.split(",");
+		this.posx = posx;
+		this.posy = posy;
+	}
+	
+}
