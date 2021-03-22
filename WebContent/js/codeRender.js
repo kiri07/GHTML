1462e069e1eea5a42648dcd4274ff2df2aa0b379
kiri07 <merlinalessandroandrea@gmail.com>
2021-03-22 18:59:37 +0100

    Update codeRender.js

--- WebContent/js/codeRender.js
+++ WebContent/js/codeRender.js
@@ -5,8 +5,7 @@ var CodeRenderer =
 			tab:
 				"<div class=tab></div>",
 			keywords:
-				['abstract','finally','protected','boolean','float','public','break','for','return','byte','goto','short','case','if','static','catch','implements','super','char','import','switch','class','instanceof','synchronized','const','int','this','continue','interface','throw','default','long','throws','do','native','transient','double','new','try','else','null','void','extends','package','volatile','final','private','while'
-],
+				["abstract", "default", "super", "for", "while", "finally", "case", "return", "default", "break", "try", "catch", "new ", "if(", "else", "switch", "do  ", "while", "int ", "double ", "float ", "boolean ", "char ", "short ", "long ", "byte ", "package ", "class ", "private ", "this ", "public ", "static ", "void"],
 			variables:
 				[],
 			props:
