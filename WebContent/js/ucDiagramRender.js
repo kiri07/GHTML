ed4062e9aa33cf403e59596fd1be9d08c4dad232
kiri07 <merlinalessandroandrea@gmail.com>
2021-03-23 11:35:39 +0100

    betterRender

--- WebContent/js/ucDiagramRender.js
+++ WebContent/js/ucDiagramRender.js
@@ -1,12 +1,16 @@
 var UCRenderer =
-			{
-				render:function(actor)
-				{
-					var actorName = actor.attr('name');
-					var res = "<div class=w3-col m1'style='margin-right:16px'>"+actorName;
-					var fields = actor.attr('uses').split(",");
-					res+="<div class='field w3-col m3' style='display:inline-block;margin-left:16px'>"+fields+"</div>";
-					res+="</div>"
-					return res;
-				}
-			}
+            {
+                render:function(actor)
+                {
+                    var actorName = actor.attr('name');
+                    var res = "<div class='ucRow'><div class='actor'>"+actorName+"</div>";
+                    var fields = actor.attr('uses').split(",");
+                    res+="<div class='ucfield'>";
+					for (var i = 0; i < fields.length ; i ++)
+						res += fields[i] + "\n";
+					res += "</div></div>";
+                    console.log(res);
+
+                    return res;
+                }
+            }
