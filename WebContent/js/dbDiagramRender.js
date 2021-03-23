2229fb2603cb7f7a6c65f7d20e232b7d7d9850fc
kiri07 <merlinalessandroandrea@gmail.com>
2021-03-23 15:34:06 +0100

    formatted

--- WebContent/js/dbDiagramRender.js
+++ WebContent/js/dbDiagramRender.js
@@ -7,28 +7,26 @@ var DBRenderer =
 				rendertable:function(table)
 				{
 				
-					var res = "<div class='dbdiagram col' style='display:inline-block;padding:1rem'>"
+					var res = "<div class='dbdiagram bg-warning col' style='display:inline-block;padding:1rem'>"
 					var name = table.attr('name');
 					var fields = table.attr('fields').split(",");
-					res += "<div class='col'>"+"<div class=field style='font-weight:bold'>"+name+"</div>";
+					res += "<div class=field style='font-weight:bold'>"+name+"</div>";
 					res += "<hr style='height:3px;border:none;color:#333;background-color:#333;'>";
 					for(var i=0;i<fields.length;i++)
 						res+="<div class=field>"+fields[i]+"</div>";
-					res+="</div>"+"</div>"
+					res+="</div>";
 					return res;
 				},
 				renderrel:function(rel)
 				{
-					var res = "<div class='dbrel col'>";
+					var res = "<div class='dbrel bg-danger col'>";
 					var left = rel.attr('left');
 					var right = rel.attr('right');
 					var join = rel.attr('join');
-					res += "<div style='text-align:center'>"+left+":"+right + "</div>";
+					res += "<div style='text-align:center;padding-top:50%'>"+left+":"+right + "</div>";
 					res += "<div>"+join + "</div>";
 					res += "<hr style='height:3px;border:none;color:#333;background-color:#333;'>";
 					res += "</div>"
-					
-					res += "<div class=arrow>" + "</div>";
 					return res;
 				}
 			}
