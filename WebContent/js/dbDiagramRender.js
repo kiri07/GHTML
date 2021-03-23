1b9d27c15ab14af47c63018422b302abbded913c
kiri07 <merlinalessandroandrea@gmail.com>
2021-03-23 14:00:30 +0100

    css update dbDia

--- WebContent/js/dbDiagramRender.js
+++ WebContent/js/dbDiagramRender.js
@@ -7,18 +7,28 @@ var DBRenderer =
 				rendertable:function(table)
 				{
 				
+					var res = "<div class='dbdiagram w3-col' style='display:inline-block;padding:1rem'>"
 					var name = table.attr('name');
 					var fields = table.attr('fields').split(",");
-					var res = "<div class=w3-col m1'>"+name;
+					res += "<div class=w3-col m1'>"+"<div class=field style='font-weight:bold'>"+name+"</div>";
+					res += "<hr style='height:3px;border:none;color:#333;background-color:#333;'>";
 					for(var i=0;i<fields.length;i++)
 						res+="<div class=field>"+fields[i]+"</div>";
-					res+="</div>"
+					res+="</div>"+"</div>"
 					return res;
 				},
 				renderrel:function(rel)
 				{
+					var res = "<div class=dbrel>";
 					var left = rel.attr('left');
 					var right = rel.attr('right');
-					return left+":"+right;
+					var join = rel.attr('join');
+					res += "<div style='text-align:center'>"+left+":"+right + "</div>";
+					res += "<div>"+join + "</div>";
+					res += "<hr style='height:3px;border:none;color:#333;background-color:#333;'>";
+					res += "</div>"
+					
+					res += "<div class=arrow>" + "</div>";
+					return res;
 				}
 			}
