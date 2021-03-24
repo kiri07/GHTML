a290f8d26b4efc203b7f21e918242348a91d6726
bobbysfish <74557350+bobbysfish@users.noreply.github.com>
2021-03-24 14:31:31 +0100

    Update ucDiagramRender.js

--- WebContent/js/ucDiagramRender.js
+++ WebContent/js/ucDiagramRender.js
@@ -4,7 +4,7 @@ var UCRenderer =
                 {   var maxlength=uses.length>actors.length? uses.length : actors.length;
                     var a=Math.floor(maxlength/actors.length);
                     var b=Math.floor(maxlength/uses.length);
-                    var res="<canvas class='gigio' width='1500' height='"+maxlength*250+"' style='border:1px solid #d3d3d3;'></canvas>";
+                    var res="<canvas class='gigio' width='1500' height='"+maxlength*240+"' style='border:1px solid #d3d3d3;'></canvas>";
                     
 
                     for (var i = 1; i <= maxlength; i ++)
@@ -13,18 +13,17 @@ var UCRenderer =
                         res+= "<div class='ucRow'><div class='emptycol'></div><div class='"+(((i%a)==0 &&(Math.floor(i/a)<=actors.length))?("actor'>"+"<img src='stickman.jpeg' style='width:50%;'></img>"+actors[Math.floor(i/a)-1])
                             :"emptycol'>")+"</div>";
                    
-                        res+="<div class='emptycol' ;>";
+                        res+="<div class='emptycol'>";
                         //for(var a = 0; a < map.length; a ++)
                        
                         //if(map[a][0]==actors[Math.floor(i/a)-1]&&(i%a)==0 &&(Math.floor(i/a)<=actors.length))
-                        res+="<div class='dbrel'></div><div></div><canvas class='gigi' style='width:100%;'></canvas>";
+                        res+="<div class='ucrel'></div><div></div><canvas class='gigi' style='width:100%;'></canvas>";
                         res+="</div><div class='"+(((i%b)==0 &&(Math.floor(i/b)<=uses.length))?("ucfield'>"+uses[Math.floor(i/b)-1]):"emptycol'>");
                     
                         res += "</div></div>";
                     }
-                  
-
-
+                    res+="<div class='ucRow'><div class='emptycol'></div></div>";
+                     res+="<div class='ucRow'><div class='emptycol'></div></div>";
                     return res;
                 }
             }
