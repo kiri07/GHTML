8fdc2bfb317c97faafdf1b46c44068f4f59afaa5
bobbysfish <74557350+bobbysfish@users.noreply.github.com>
2021-03-24 12:37:26 +0100

    Update ucDiagramRender.js

--- WebContent/js/ucDiagramRender.js
+++ WebContent/js/ucDiagramRender.js
@@ -4,24 +4,26 @@ var UCRenderer =
                 {   var maxlength=uses.length>actors.length? uses.length : actors.length;
                     var a=Math.floor(maxlength/actors.length);
                     var b=Math.floor(maxlength/uses.length);
-                    var res="";
+                    var res="<canvas class='gigio' width='1500' height='"+maxlength*250+"' style='border:1px solid #d3d3d3;'></canvas>";
                     
+
                     for (var i = 1; i <= maxlength; i ++)
                     {
                     
-                        res+= "<div class='ucRow'><div class='"+(((i%a)==0 &&(Math.floor(i/a)<=actors.length))?("actor'>"+actors[Math.floor(i/a)-1]):"emptycol'>")+"</div>";
+                        res+= "<div class='ucRow'><div class='emptycol'></div><div class='"+(((i%a)==0 &&(Math.floor(i/a)<=actors.length))?("actor'>"+"<img src='stickman.jpeg' style='width:50%;'></img>"+actors[Math.floor(i/a)-1])
+                            :"emptycol'>")+"</div>";
                    
-                        res+="<div class='emptycol'>";
+                        res+="<div class='emptycol' ;>";
                         //for(var a = 0; a < map.length; a ++)
                        
                         //if(map[a][0]==actors[Math.floor(i/a)-1]&&(i%a)==0 &&(Math.floor(i/a)<=actors.length))
-                         //   res+="<dbel type='rel' left='1' right='n' join='author.id=book.authorid'></dbel>";
-
-                        res+="</div><div class='"+(((i%b)==0 &&(Math.floor(i/b)<=uses.length))?("ucfield'>"+uses[Math.floor(i/b)-1]):"emptycol'>")+"</div>";
-					
-					    res += "</div>";
-                    }
+                        res+="<div class='dbrel'></div><div></div><canvas class='gigi' style='width:100%;'></canvas>";
+                        res+="</div><div class='"+(((i%b)==0 &&(Math.floor(i/b)<=uses.length))?("ucfield'>"+uses[Math.floor(i/b)-1]):"emptycol'>");
                     
+                        res += "</div></div>";
+                    }
+                  
+
 
                     return res;
                 }
