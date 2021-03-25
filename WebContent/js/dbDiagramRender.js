59ac227b52ae4dbc9d1e694cddd03ec9cf0de005
kiri07 <merlinalessandroandrea@gmail.com>
2021-03-25 18:24:36 +0100

    xsd and better db graph

--- WebContent/js/dbDiagramRender.js
+++ WebContent/js/dbDiagramRender.js
@@ -6,25 +6,29 @@ var DBRenderer =
 				},
 				rendertable:function(table)
 				{
-					var res = "<div class='dbdiagram col'>";
+					var res = "<div class='dbdiagram col' style='margin:0'>";
                     var name = table.attr('name');
                     var fields = table.attr('fields').split(",");
                     res += "<div class='row' style='background-color:lightblue'>";
-                    res += "<div class='db field name'>"+name+"</div>"+"</div>";
-                    var key = "<i class='fa fa-key' aria-hidden='true'></i>";
+                    res += "<div class='db field name mx-auto'>"+name+"</div>"+"</div>";
+                    var key = "<i class='fa fa-key mr-2' aria-hidden='true'></i>";
+					var elementsImage= "<i class='far fa-gem mr-2'></i>";
                     //res += "<hr style='height:3px;border:none;color:#333;background-color:#333;'>";
                     for(var i=0;i<fields.length;i++)
-                    	{	
+                    {	
                     	var con=fields[i].includes('pk');
                     	console.log(con);
-                        res+="<div class=field>"+(fields[i].includes('pk') ? key+" " : "")+fields[i]+"</div>";
+						if(fields[i].includes('pk'))
+                        	res+="<div class='field'>"+ key + fields[i]+"</div>";
+						else
+							res+="<div class='field'>"+ elementsImage + fields[i]+"</div>";
                     }
                     res+="</div>";
                     return res;
 				},
 				renderrel:function(rel)
 				{
-					var res = "<div class='col'>";
+					var res = "<div class='col' style='padding:0'>";
 					var left = rel.attr('left');
 					var right = rel.attr('right');
 					var join = rel.attr('join');
@@ -33,7 +37,11 @@ var DBRenderer =
 					// IN DOCUMENT READY
 					// res += "<div class='dbrel'>"+left+":"+right + "</div>";
 					// res += "<div>"+join + "</div>";
-					res += "<canvas class='dbrel' left="+left+" right=" + right + " join="+join+" style='width:100%;height:50%'></canvas>";
+					//res += "<canvas class='dbrel' left="+left+" right=" + right + " join="+join+" style='col'></canvas>";
+					res+=	"<div id='container' class='col-3'><p style='text-align:center;'>"+left+"</p></div>"
+							+"<div id='container' class='col-6'><p style='text-align:center;'>"+join+"<p></div>"
+							+"<div id='container' class='col-3'><p style='text-align:center;'>"+right+"</p></div>"
+							+"<canvas class='dbRelArrow' style='width:100%'>"+"hello im an arrow"+"</canvas>";
 					res += "</div>"
 					return res;
 				}
